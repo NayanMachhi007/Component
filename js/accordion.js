18dc2f0cdd9b36f0943246360f8a506b22a99e93
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-01-23 10:47:59 +0000

    new code updated now

--- js/accordion.js
+++ js/accordion.js
@@ -6,25 +6,3 @@ for (let i = 0; i < accordion.length; i++) {
     }
     )
 }
-
-
-// const accordionItems = document.querySelectorAll('.accordion-iteam-main');
- 
-// accordionItems.forEach(item => {
-//     const title = item.querySelector('.accordion-lable');
-//     const content = item.querySelector('.accordion-content');
- 
-//     title.addEventListener('click', () => {
-//         for (i = 0; i < accordionItems.length; i++) {
-//             if (accordionItems[i] == item) {
-//                 item.classList.toggle('active');
- 
-//             } else {
-//                 // toggle the accordion item
-//                 accordionItems[i].classList.remove('active');
-//             }
-//         }
-//     });
-// });
-
-
